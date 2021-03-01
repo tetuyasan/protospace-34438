ffb76a8ed85f2ccd58f667dc9d27a6acffa69bc9
tetuyasan <nukunukuohuton@gmail.com>
2021-03-01 17:15:32 +0900

    コミット

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:edit,:show]
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
@@ -61,7 +61,7 @@ class PrototypesController < ApplicationController
   end
 
   def contributor_confirmation
-    redirect_to root_path unless current_user.id == @prototype.user.id
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
 
